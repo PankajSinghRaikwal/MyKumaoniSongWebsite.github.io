cd42d03d22e550cec698dd58cb074a840778a5b0
Pankaj20202024 <121535589+Pankaj20202024@users.noreply.github.com>
2023-02-25 23:37:37 +0530

    commit change

---
+++ kumaon.js
@@ -0,0 +1,14 @@
+function playsong(event){
+    event.target.play();
+}
+function pausesong(event){
+    event.target.pause();
+}
+
+function playaudiosong(event){
+    event.target.play();
+}
+function pauseaudiosong(event){
+    event.target.pause();
+}
+
